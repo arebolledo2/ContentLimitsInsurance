5b902feaedd59b1113e1884abab75b6e66825833
Andres Rebolledo <andres.rebolledo@jplease.com>
2022-08-11 12:29:58 -0600

    Add domain model

---
+++ DomainModel/Content.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DomainModel
+{
+    public class Content
+    {
+        public int Id { get; set; }
+        public Category Category { get; set; }
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+        public decimal Value { get; set; }
+    }
+}
