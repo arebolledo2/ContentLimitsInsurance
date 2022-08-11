7b7397eadea32e19f5f6cc0e1a2a261b32ba74d4
Andres Rebolledo <andres.rebolledo@jplease.com>
2022-08-11 13:23:02 -0600

    Add ContentRepository

---
+++ DataAccess/ContentRepository.cs
@@ -0,0 +1,34 @@
+﻿using DomainModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class ContentRepository
+    {
+        private ContentsLimitInsuranceContext _context;
+
+        public ContentRepository(ContentsLimitInsuranceContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(Content content)
+        {
+            _context.Add(content);
+        }
+
+        public void Remove(Content content)
+        {
+            _context.Remove(content);
+        }
+
+        public async Task SaveChangesAsync(CancellationToken token)
+        {
+            await _context.SaveChangesAsync(token);
+        }
+    }
+}
