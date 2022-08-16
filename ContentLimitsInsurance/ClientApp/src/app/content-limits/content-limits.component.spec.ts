351e41e7d36b4e9f50d1c142954b64dd5dd79532
Andres Rebolledo <andres@rtbsoftware.com>
2022-08-16 12:23:08 -0600

    Test category display WIP

--- ContentLimitsInsurance/ClientApp/src/app/content-limits/content-limits.component.spec.ts
+++ ContentLimitsInsurance/ClientApp/src/app/content-limits/content-limits.component.spec.ts
@@ -6,7 +6,6 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-
 import { ContentLimitsComponent } from './content-limits.component';
 import { IContentLimitsService } from './content-limits.service'
 import { MockContentLimitsService } from './mock.content-limits.service'
@@ -39,24 +38,27 @@ describe('ContentLimitsComponent', () => {
         fixture.detectChanges();
     });
 
-    ////it('should display categories', async(() => {
-    ////    const titleText = fixture.nativeElement.querySelector('h1').textContent;
-    ////    expect(titleText).toEqual('Counter');
-    ////}));
+    it('should display categories and contents', async(() => {
+        expect(fixture.nativeElement).toContain("Category 1");
+        expect(fixture.nativeElement).toContain("Category 2");
+        expect(fixture.nativeElement).toContain("Content 1");
+        expect(fixture.nativeElement).toContain("Content 2");
+        expect(fixture.nativeElement).toContain("Content 3");
+    }));
 
-    ////it('should use mock service', async(() => {
-    ////    expect(fixture.componentInstance.service).toBe(TestBed.get(IContentLimitsService));
-    ////}));
+    it('should use mock service', async(() => {
+        expect(fixture.componentInstance.service).toBe(TestBed.get(IContentLimitsService));
+    }));
 
-    it('should display a total', async(() => {
+    it('should display the correct total', async(() => {
         const total = fixture.nativeElement.querySelector('#total').textContent;
         expect(total).toEqual('$600');
     }));
 
-    ////it('should add', async(() => {
-    ////    const titleText = fixture.nativeElement.querySelector('h1').textContent;
-    ////    expect(titleText).toEqual('Counter');
-    ////}));
+    it('should add', async(() => {
+        const titleText = fixture.nativeElement.querySelector('h1').textContent;
+        expect(titleText).toEqual('Counter');
+    }));
 
     ////it('should delete', async(() => {
     ////    const titleText = fixture.nativeElement.querySelector('h1').textContent;
